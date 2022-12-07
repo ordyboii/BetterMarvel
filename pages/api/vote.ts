47a194384ea8c18f5d2953ebdedf71145a0f1a54
Jake <jake.ord345@gmail.com>
2022-12-07 23:20:36 +0000

    post

---
+++ pages/api/vote.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+import { prisma } from "@server/db";
+
+const voteSchema = z.object({
+  voteForId: z.string(),
+  voteAgainstId: z.string()
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { voteForId, voteAgainstId } = voteSchema.parse(await req.body());
+
+  await prisma.vote.create({
+    data: {
+      votedAgainst: { connect: { id: voteAgainstId } },
+      votedFor: { connect: { id: voteForId } }
+    }
+  });
+
+  res.json({ success: true });
+}
