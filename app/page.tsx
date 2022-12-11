ef3224d006edc7bcdcaed74727d6d1726a6b7e83
Jake <jake.ord345@gmail.com>
2022-12-11 20:21:02 +0000

    change?

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getMovies } from "@server/movies";
 import MovieCard from "./movie-card";
 
+export const dynamic = "force-dynamic";
+
 export default async function RootPage() {
   const movies = await getMovies();
 
